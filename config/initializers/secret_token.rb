d1370b874ac23fdcf64b4b15f65b9ca1dbfa66e5
Mason Glidden <mglidden@mit.edu>
2012-02-06 14:56:17 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanfic::Application.config.secret_token = '2acd78f6304e776abe54db9dab19a56dfedf5623b31fa84f810e4823b92bcd9006c97e256373178ded327353288d56f815515aa58e12ba8386f2108a19f81ec8'
