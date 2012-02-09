2d2dc2d5d88e9e316e1488864078ea99f151996f
Mason Glidden <mglidden@mit.edu>
2012-02-09 00:44:24 -0500

    changing around the root of the site

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Fanfic::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'stories#current'
 
   # See how all your routes lay out with "rake routes"
 
