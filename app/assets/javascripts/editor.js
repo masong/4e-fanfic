94769328f25824e9f1f9f696329bcc2240ca6522
Mason Glidden <mglidden@mit.edu>
2012-02-12 18:03:52 -0500

    editor box dissapears when you run out of time

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -11,3 +11,10 @@ editor.inputChange = function(event) {
   var input = event.target.value;
   $('.story-body').html(editor.originalText + input.replace(/\n/g, '<br />'));
 };
+
+editor.hideInput = function() {
+  $('#story-controls').fadeSliderToggle({callback: function() {
+    $('#story-controls').text('Your time has expired. You must wait for someone else to edit before you can contribute again.');
+    $('#story-controls').fadeSliderToggle({speed:200});
+  }});
+};
