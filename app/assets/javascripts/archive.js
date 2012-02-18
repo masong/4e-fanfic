c067fd5fed4146539769c63c0b8e825a5f29c9b7
Mason Glidden <mglidden@mit.edu>
2012-02-17 23:57:14 -0500

    put little arrow next to the title, improved strength of the toggle fn

--- app/assets/javascripts/archive.js
+++ app/assets/javascripts/archive.js
@@ -1,9 +1,9 @@
 archive = {}
 
 archive.toggle = function() {
-  var target = $(window.event.target);
-  if (target.attr('class') == 'date') {
-    target = target.parent();
-  }
-  target.next().children(0).slideToggle();
+  var target = $(window.event.target).parents('.story');
+  target.children(':nth-child(2)').children(':first').slideToggle();
+  target.children(':first').children(':first').toggleClass('arrow-rotate-down');
+  window.console.log(window.event.target);
+  window.console.log(window.event.target.parentNode);
 };
