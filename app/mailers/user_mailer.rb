880b56b15fb3ce87bc05b7c4fb7def28b685a4f4
Mason Glidden <mglidden@mit.edu>
2012-02-21 22:46:49 -0500

    actually making the email bcc and putting something in the to field

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ActionMailer::Base
   def fanfic_open(story)
     @emails = User.all.find_all {|u| u.email_alerts}.collect {|u| u.email}
     @url = 'http://localhost/'
-    mail(:cc => @emails, :subject => 'Fanfic is now open again', :body => 'Visit https://slugfic.mit.edu to contribute.')
+    mail(:to => 'slugfest.fanfic@gmail.com', :bcc => @emails, :subject => 'Fanfic is now open again', :body => 'Visit https://slugfic.mit.edu to contribute.')
   end
 end
